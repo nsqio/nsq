844a089661868cd6bdf4c70fadabcb231503296a
Matt Reiferson <mreiferson@gmail.com>
2012-05-25 15:06:23 -0400

    version and help args

--- nsqd/nsqd.go
+++ nsqd/nsqd.go
@@ -11,17 +11,39 @@ import (
 	"strconv"
 )
 
-var bindAddress = flag.String("address", "", "address to bind to")
-var webPort = flag.Int("web-port", 5150, "port to listen on for HTTP connections")
-var tcpPort = flag.Int("tcp-port", 5151, "port to listen on for TCP connections")
-var debugMode = flag.Bool("debug", false, "enable debug mode")
-var memQueueSize = flag.Int("mem-queue-size", 10000, "number of messages to keep in memory (per topic)")
-var cpuProfile = flag.String("cpu-profile", "", "write cpu profile to file")
-var goMaxProcs = flag.Int("go-max-procs", 4, "runtime configuration for GOMAXPROCS")
-var dataPath = flag.String("data-path", "", "path to store disk-backed messages")
+const VERSION = "0.1"
+
+var (
+	showVersion     = flag.Bool("version", false, "print version string")
+	showHelp        = flag.Bool("help", false, "print help")
+	bindAddress     = flag.String("address", "0.0.0.0", "address to bind to")
+	webPort         = flag.Int("web-port", 5150, "port to listen on for HTTP connections")
+	tcpPort         = flag.Int("tcp-port", 5151, "port to listen on for TCP connections")
+	debugMode       = flag.Bool("debug", false, "enable debug mode")
+	memQueueSize    = flag.Int("mem-queue-size", 10000, "number of messages to keep in memory (per topic)")
+	cpuProfile      = flag.String("cpu-profile", "", "write cpu profile to file")
+	goMaxProcs      = flag.Int("go-max-procs", 4, "runtime configuration for GOMAXPROCS")
+	dataPath        = flag.String("data-path", "", "path to store disk-backed messages")
+	lookupAddresses = util.StringArray{}
+)
+
+func init() {
+	flag.Var(&lookupAddresses, "lookup-address", "lookup address (may be given multiple times)")
+}
 
 func main() {
 	flag.Parse()
+	
+	if *showVersion {
+		fmt.Printf("nsqd v%s\n", VERSION)
+		return
+	}
+	
+	if *showHelp {
+		fmt.Printf("nsqd v%s\n", VERSION)
+		flag.PrintDefaults()
+		return
+	}
 
 	if *goMaxProcs > 0 {
 		runtime.GOMAXPROCS(*goMaxProcs)
---
+++ util/string_array.go
@@ -0,0 +1,12 @@
+package util
+
+type StringArray []string
+
+func (a *StringArray) Set(s string) error {
+	*a = append(*a, s)
+	return nil
+}
+
+func (a *StringArray) String() string {
+	return fmt.Sprint(*a)
+}
