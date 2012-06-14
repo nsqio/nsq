9aff86f727e4fc90114ac08ea051a94a329705b3
Matt Reiferson <mreiferson@gmail.com>
2012-06-14 18:54:45 +0000

    dont rely on time for multiple consumer test

--- nsqd/server_protocol_v2_test.go
+++ nsqd/server_protocol_v2_test.go
@@ -8,7 +8,6 @@ import (
 	// "log"
 	"net"
 	"testing"
-	"time"
 )
 
 // exercise the basic operations of the V2 protocol
@@ -18,6 +17,7 @@ func TestBasicV2(t *testing.T) {
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:5170")
 	tcpListener, err := net.Listen("tcp", tcpAddr.String())
+	assert.Equal(t, err, nil)
 	defer tcpListener.Close()
 
 	go TopicFactory(10, ".")
@@ -26,21 +26,21 @@ func TestBasicV2(t *testing.T) {
 	msg := nsq.NewMessage(util.Uuid(), []byte("test body"))
 	topic := GetTopic("test_v2")
 	topic.PutMessage(msg)
-
+	
 	consumer := nsq.NewConsumer(tcpAddr)
-
+	
 	err = consumer.Connect()
 	assert.Equal(t, err, nil)
-
+	
 	err = consumer.Version(nsq.ProtocolV2Magic)
 	assert.Equal(t, err, nil)
-
+	
 	err = consumer.WriteCommand(consumer.Subscribe("test_v2", "ch"))
 	assert.Equal(t, err, nil)
-
+	
 	err = consumer.WriteCommand(consumer.Ready(1))
 	assert.Equal(t, err, nil)
-
+	
 	resp, err := consumer.ReadResponse()
 	assert.Equal(t, err, nil)
 	frameType, msgInterface, err := consumer.UnpackResponse(resp)
@@ -53,12 +53,13 @@ func TestBasicV2(t *testing.T) {
 
 func TestMultipleConsumerV2(t *testing.T) {
 	// log.SetOutput(ioutil.Discard)
+	// defer log.SetOutput(os.Stdout)
 
-	readyChan := make(chan int)
 	msgChan := make(chan *nsq.Message)
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:5170")
 	tcpListener, err := net.Listen("tcp", tcpAddr.String())
+	assert.Equal(t, err, nil)
 	defer tcpListener.Close()
 
 	go TopicFactory(10, ".")
@@ -66,6 +67,9 @@ func TestMultipleConsumerV2(t *testing.T) {
 
 	msg := nsq.NewMessage(util.Uuid(), []byte("test body"))
 	topic := GetTopic("test_multiple_v2")
+	topic.GetChannel("ch1")
+	topic.GetChannel("ch2")
+	topic.PutMessage(msg)
 
 	for _, i := range []string{"1", "2"} {
 		consumer := nsq.NewConsumer(tcpAddr)
@@ -81,22 +85,12 @@ func TestMultipleConsumerV2(t *testing.T) {
 		err = consumer.WriteCommand(consumer.Ready(1))
 		assert.Equal(t, err, nil)
 
-		go func() {
-			<-time.After(time.Duration(time.Second))
-			readyChan <- 1
-		}()
-
 		go func(c *nsq.Consumer) {
 			resp, _ := c.ReadResponse()
 			_, msgInterface, _ := c.UnpackResponse(resp)
 			msgChan <- msgInterface.(*nsq.Message)
 		}(consumer)
 	}
-
-	<-readyChan
-	<-readyChan
-
-	topic.PutMessage(msg)
 
 	msgOut := <-msgChan
 	assert.Equal(t, msgOut.Uuid, msg.Uuid)
