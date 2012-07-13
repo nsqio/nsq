d8e8edd295f60bf3a0e389619aa45bb7fbeba042
Matt Reiferson <mreiferson@gmail.com>
2012-07-13 16:09:15 -0400

    add priority queue utility type

---
+++ util/pqueue/pqueue.go
@@ -0,0 +1,54 @@
+package pqueue
+
+type Item struct {
+	Value    interface{}
+	Priority int64
+	Index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].Priority > pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	// To simplify indexing expressions in these methods, we save a copy of the
+	// slice object. We could instead write (*pq)[i].
+	a := *pq
+	n := len(a)
+	a = a[0 : n+1]
+	item := x.(*Item)
+	item.Index = n
+	a[n] = item
+	*pq = a
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	a := *pq
+	n := len(a)
+	item := a[n-1]
+	item.Index = -1 // for safety
+	*pq = a[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) Peek() interface{} {
+	a := *pq
+	n := len(a)
+	item := a[n-1]
+	return item
+}
