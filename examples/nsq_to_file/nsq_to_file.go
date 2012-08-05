bf4e384cd23a9d4c28021795e59b66cf17655ff2
Matt Reiferson <mreiferson@gmail.com>
2012-08-05 14:28:49 -0400

    nsq_to_file: reorganize

--- examples/nsq_to_file/nsq_to_file.go
+++ examples/nsq_to_file/nsq_to_file.go
@@ -52,6 +52,90 @@ func (l *FileLogger) HandleMessage(m *nsq.Message, responseChannel chan *nsq.Fin
 	l.logChan <- &Message{m, responseChannel}
 }
 
+func router(r *nsq.Reader, f *FileLogger, termChan chan os.Signal, hupChan chan os.Signal) {
+	pos := 0
+	output := make([]*SyncMsg, *buffer)
+	sync := false
+	ticker := time.Tick(time.Duration(30) * time.Second)
+
+	for {
+		select {
+		case <-termChan:
+			r.Stop()
+			sync = true
+		case <-hupChan:
+			f.out.Close()
+			f.out = nil
+			updateFile(f)
+			if pos != 0 {
+				sync = true
+			}
+		case <-ticker:
+			if pos != 0 || f.out != nil {
+				updateFile(f)
+				sync = true
+			}
+		case m := <-f.logChan:
+			if updateFile(f) {
+				sync = true
+			}
+			f.out.Write(m.Body)
+			f.out.WriteString("\n")
+			x := &nsq.FinishedMessage{m.Id, 0, true}
+			output[pos] = &SyncMsg{x, m.returnChannel}
+			pos++
+		}
+
+		// in the case where you have N connections, flush after the 
+		// smallest buffer size for a single connection (otherwise the async handler will wait to finish message)
+		// and you will starve your connection
+		if sync || pos >= *buffer || pos >= r.ConnectionBufferSize() {
+			if pos > 0 {
+				log.Printf("syncing %d records to disk", pos)
+				f.out.Sync()
+				for pos > 0 {
+					pos--
+					m := output[pos]
+					m.returnChannel <- m.m
+					output[pos] = nil
+				}
+			}
+			sync = false
+		}
+	}
+}
+
+func updateFile(f *FileLogger) bool {
+	t := time.Now()
+	hostname, _ := os.Hostname()
+	shortHostname := strings.Split(hostname, ".")[0]
+	identifier := shortHostname
+	if len(*hostIdentifier) != 0 {
+		identifier = strings.Replace(*hostIdentifier, "<SHORT_HOST>", shortHostname, -1)
+		identifier = strings.Replace(identifier, "<HOSTNAME>", hostname, -1)
+	}
+	filename := fmt.Sprintf(filenamePattern, *topic, identifier, t.Year(), t.Month(), t.Day(), t.Hour())
+
+	if filename != f.filename || f.out == nil {
+		log.Printf("old %s new %s", f.filename, filename)
+		// roll it
+		if f.out != nil {
+			f.out.Close()
+		}
+		os.MkdirAll(*outputDir, 777)
+		log.Printf("opening %s/%s", *outputDir, filename)
+		newfile, err := os.OpenFile(fmt.Sprintf("%s/%s", *outputDir, filename), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		f.out = newfile
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.filename = filename
+		return true
+	}
+
+	return false
+}
+
 func main() {
 	flag.Parse()
 
@@ -64,10 +148,10 @@ func main() {
 	}
 
 	if len(nsqAddresses) == 0 && len(lookupdAddresses) == 0 {
-		log.Fatalf("--nsq-address or --lookupd-address required.")
+		log.Fatalf("--nsqd-tcp-address or --lookupd-http-address required.")
 	}
 	if len(nsqAddresses) != 0 && len(lookupdAddresses) != 0 {
-		log.Fatalf("use --nsq-address or --lookupd-address not both")
+		log.Fatalf("use --nsqd-tcp-address or --lookupd-http-address not both")
 	}
 
 	hupChan := make(chan os.Signal, 1)
@@ -84,57 +168,7 @@ func main() {
 	r.VerboseLogging = *verbose
 
 	r.AddAsyncHandler(f)
-	go func() {
-		var pos = 0
-		var output = make([]*SyncMsg, *buffer)
-		var sync = false
-		var ticker = time.Tick(time.Duration(30) * time.Second)
-		for {
-			select {
-			case <-termChan:
-				r.Stop()
-				sync = true
-			case <-hupChan:
-				f.out.Close()
-				f.out = nil
-				updateFile(f)
-				if pos != 0 {
-					sync = true
-				}
-			case <-ticker:
-				if pos != 0 || f.out != nil {
-					updateFile(f)
-					sync = true
-				}
-			case m := <-f.logChan:
-				if updateFile(f) {
-					sync = true
-				}
-				f.out.Write(m.Body)
-				f.out.WriteString("\n")
-				x := &nsq.FinishedMessage{m.Id, 0, true}
-				output[pos] = &SyncMsg{x, m.returnChannel}
-				pos++
-			}
-
-			// in the case where you have N connections, flush after the 
-			// smallest buffer size for a single connection (otherwise the async handler will wait to finish message)
-			// and you will starve your connection
-			if sync || pos >= *buffer || pos >= r.ConnectionBufferSize() {
-				if pos > 0 {
-					log.Printf("syncing %d records to disk", pos)
-					f.out.Sync()
-					for pos > 0 {
-						pos--
-						m := output[pos]
-						m.returnChannel <- m.m
-						output[pos] = nil
-					}
-				}
-				sync = false
-			}
-		}
-	}()
+	go router(r, f, termChan, hupChan)
 
 	for _, addrString := range nsqAddresses {
 		err := r.ConnectToNSQ(addrString)
@@ -152,35 +186,4 @@ func main() {
 	}
 
 	<-r.ExitChan
-
-}
-
-func updateFile(f *FileLogger) bool {
-	t := time.Now()
-
-	hostname, _ := os.Hostname()
-	shortHostname := strings.Split(hostname, ".")[0]
-	identifier := shortHostname
-	if len(*hostIdentifier) != 0 {
-		identifier = strings.Replace(*hostIdentifier, "<SHORT_HOST>", shortHostname, -1)
-		identifier = strings.Replace(identifier, "<HOSTNAME>", hostname, -1)
-	}
-	filename := fmt.Sprintf(filenamePattern, *topic, identifier, t.Year(), t.Month(), t.Day(), t.Hour())
-	if filename != f.filename || f.out == nil {
-		log.Printf("old %s new %s", f.filename, filename)
-		// roll it
-		if f.out != nil {
-			f.out.Close()
-		}
-		os.MkdirAll(*outputDir, 777)
-		log.Printf("opening %s/%s", *outputDir, filename)
-		newfile, err := os.OpenFile(fmt.Sprintf("%s/%s", *outputDir, filename), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-		f.out = newfile
-		if err != nil {
-			log.Fatal(err)
-		}
-		f.filename = filename
-		return true
-	}
-	return false
 }
