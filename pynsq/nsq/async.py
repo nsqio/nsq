e5107e7677f3805a254a684124ef0c338f36e803
Matt Reiferson <mreiferson@gmail.com>
2012-08-14 15:16:50 -0400

    pynsq: low-level python client for nsq

---
+++ pynsq/nsq/async.py
@@ -0,0 +1,91 @@
+import socket
+import struct
+
+import tornado.iostream
+
+import nsq
+
+
+class AsyncConn(object):
+    def __init__(self, host, port, connect_callback, data_callback, close_callback, timeout=1.0):
+        assert isinstance(host, (str, unicode))
+        assert isinstance(port, int)
+        assert callable(data_callback)
+        assert callable(close_callback)
+        assert isinstance(timeout, float)
+        
+        self.connecting = False
+        self.connected = False
+        self.host = host
+        self.port = port
+        self.connect_callback = connect_callback
+        self.data_callback = data_callback
+        self.close_callback = close_callback
+        self.timeout = timeout
+    
+    def connect(self):
+        if self.connected or self.connecting:
+            return
+        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(self.timeout)
+        self.s.setblocking(0)
+        
+        self.stream = tornado.iostream.IOStream(self.s)
+        self.stream.set_close_callback(self._socket_close)
+        
+        self.connecting = True
+        self.stream.connect((self.host, self.port), self._connect_callback)
+    
+    def _connect_callback(self):
+        self.connecting = False
+        self.connected = True
+        self.stream.write(nsq.MAGIC_V2)
+        self.stream.read_bytes(4, self._read_size)
+        self.connect_callback(self)
+    
+    def _socket_close(self):
+        self.connected = False
+        self.close_callback(self)
+    
+    def close(self):
+        self.connected = False
+        self.stream.close()
+    
+    def _read_size(self, data):
+        size = struct.unpack('>l', data)[0]
+        self.stream.read_bytes(size, self._read_body)
+    
+    def _read_body(self, data):
+        self.stream.read_bytes(4, self._read_size)
+        self.data_callback(self, data)
+    
+    def send(self, data):
+        self.stream.write(data)
+    
+    def __str__(self):
+        return self.host + ':' + str(self.port)
+
+
+if __name__ == '__main__':
+    def connect_callback(c):
+        print "connected"
+        c.send(nsq.subscribe('test', 'ch', 'a', 'b'))
+        c.send(nsq.ready(1))
+    
+    def close_callback(c):
+        print "connection closed"
+    
+    def data_callback(c, data):
+        unpacked = nsq.unpack_response(data)
+        if unpacked[0] == nsq.FRAME_TYPE_MESSAGE:
+            c.send(nsq.ready(1))
+            msg = nsq.decode_message(unpacked[1])
+            print msg.id, msg.body
+            c.send(nsq.finish(msg.id))
+    
+    c = AsyncConn("127.0.0.1", 4150, connect_callback, data_callback, close_callback)
+    c.connect()
+    
+    tornado.ioloop.IOLoop.instance().start()
+
