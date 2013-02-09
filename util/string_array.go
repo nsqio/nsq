85427aa9177e002713d15f88136cb4a4aa57ea95
Galvin Hsiu <galvin.hsiu@gree.co.jp>
2013-02-09 01:03:56 -0800

    Fixes for string_array, allow comma delimited splitting

--- util/string_array.go
+++ util/string_array.go
@@ -1,13 +1,21 @@
 package util
 
 import (
+	"strings"
 	"fmt"
 )
 
 type StringArray []string
 
 func (a *StringArray) Set(s string) error {
-	*a = append(*a, s)
+	if strings.Contains(s, ",") {
+		for _, entry := range strings.Split(s, ",") {
+			*a = append(*a, entry)
+		}
+	} else {
+		*a = append(*a, s)
+	}
+
 	return nil
 }
 
