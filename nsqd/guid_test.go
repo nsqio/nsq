084907654f14b024eae712880be987c2d6fbcb80
Matt Reiferson <mreiferson@gmail.com>
2013-04-10 11:27:00 -0400

    update README benchmarks; add some GUID benchmarks

---
+++ nsqd/guid_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/bitly/nsq/nsq"
+	"testing"
+	"unsafe"
+)
+
+func BenchmarkGUIDCopy(b *testing.B) {
+	source := make([]byte, 16)
+	var dest nsq.MessageID
+	for i := 0; i < b.N; i++ {
+		copy(dest[:], source)
+	}
+}
+
+func BenchmarkGUIDUnsafe(b *testing.B) {
+	source := make([]byte, 16)
+	var dest nsq.MessageID
+	for i := 0; i < b.N; i++ {
+		dest = *(*nsq.MessageID)(unsafe.Pointer(&source[0]))
+	}
+	_ = dest
+}
