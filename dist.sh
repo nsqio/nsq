f4880d2716e38c8be557bebb2f88515d96257f21
Matt Reiferson <mreiferson@gmail.com>
2013-04-12 00:14:40 -0400

    fix dist script

--- dist.sh
+++ dist.sh
@@ -3,25 +3,30 @@
 # build binary distributions for linux/amd64 and darwin/amd64
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $DIR
+mkdir -p $DIR/dist
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
-version=$(cat util/binary_version.go | tail -n1 | awk '{print $NF}' | sed 's/"//g')
+version=$(cat $DIR/util/binary_version.go | tail -n1 | awk '{print $NF}' | sed 's/"//g')
 
 TMPGOPATH=$(mktemp -d -t nsqgopath)
 mkdir -p $TMPGOPATH/src
 mkdir -p $TMPGOPATH/pkg/${os}_${arch}
+mkdir -p $TMPGOPATH/src/github.com/bitly/nsq
+
+git archive HEAD | tar -x -C $TMPGOPATH/src/github.com/bitly/nsq
+
 export GOPATH="$TMPGOPATH:$GOROOT"
 
 echo "... getting dependencies"
 go get -v github.com/bitly/go-simplejson
 go get -v github.com/bmizerany/assert
 
+pushd $TMPGOPATH/src/github.com/bitly/nsq
+
 echo "... running tests"
 ./test.sh || exit 1
 
-mkdir -p dist
 for os in linux darwin; do
     echo "... building v$version for $os/$arch"
     BUILD=$(mktemp -d -t nsq)
@@ -34,3 +39,5 @@ for os in linux darwin; do
     popd
     make clean
 done
+
+popd
