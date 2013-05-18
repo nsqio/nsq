feb8a17ed7d4a515728f02d53783311524bccce6
Matt Reiferson <mreiferson@gmail.com>
2013-05-18 15:24:27 -0400

    bump to 0.2.21-alpha

--- util/binary_version.go
+++ util/binary_version.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const BINARY_VERSION = "0.2.20"
+const BINARY_VERSION = "0.2.21-alpha"
 
 func Version(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, BINARY_VERSION, runtime.Version())
