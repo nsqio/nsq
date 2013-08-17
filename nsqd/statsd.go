d79d94d7d89f8fbc031986ecc7b825a23a4ae09c
Matt Reiferson <mreiferson@gmail.com>
2013-08-17 17:07:41 -0400

    nsqd: send mem/gc stats to statsd
    * use int64 for statsd client values
    * add --statsd-mem-stats flag, default on

--- nsqd/statsd.go
+++ nsqd/statsd.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 	"github.com/bitly/nsq/util"
 	"log"
+	"math"
+	"runtime"
+	"sort"
 	"time"
 )
 
+type Uint64Slice []uint64
+
+func (s Uint64Slice) Len() int {
+	return len(s)
+}
+
+func (s Uint64Slice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Uint64Slice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
 func (n *NSQd) statsdLoop() {
+	var lastMemStats runtime.MemStats
 	lastStats := make([]TopicStats, 0)
 	ticker := time.NewTicker(n.options.statsdInterval)
 	for {
@@ -36,13 +54,13 @@ func (n *NSQd) statsdLoop() {
 				}
 				diff := topic.MessageCount - lastTopic.MessageCount
 				stat := fmt.Sprintf("topic.%s.message_count", topic.TopicName)
-				statsd.Incr(stat, int(diff))
+				statsd.Incr(stat, int64(diff))
 
 				stat = fmt.Sprintf("topic.%s.depth", topic.TopicName)
-				statsd.Gauge(stat, int(topic.Depth))
+				statsd.Gauge(stat, topic.Depth)
 
 				stat = fmt.Sprintf("topic.%s.backend_depth", topic.TopicName)
-				statsd.Gauge(stat, int(topic.BackendDepth))
+				statsd.Gauge(stat, topic.BackendDepth)
 
 				for _, channel := range topic.Channels {
 					// try to find the channel in the last collection
@@ -55,38 +73,72 @@ func (n *NSQd) statsdLoop() {
 					}
 					diff := channel.MessageCount - lastChannel.MessageCount
 					stat := fmt.Sprintf("topic.%s.channel.%s.message_count", topic.TopicName, channel.ChannelName)
-					statsd.Incr(stat, int(diff))
+					statsd.Incr(stat, int64(diff))
 
 					stat = fmt.Sprintf("topic.%s.channel.%s.depth", topic.TopicName, channel.ChannelName)
-					statsd.Gauge(stat, int(channel.Depth))
+					statsd.Gauge(stat, channel.Depth)
 
 					stat = fmt.Sprintf("topic.%s.channel.%s.backend_depth", topic.TopicName, channel.ChannelName)
-					statsd.Gauge(stat, int(channel.BackendDepth))
+					statsd.Gauge(stat, channel.BackendDepth)
 
 					stat = fmt.Sprintf("topic.%s.channel.%s.in_flight_count", topic.TopicName, channel.ChannelName)
-					statsd.Gauge(stat, int(channel.InFlightCount))
+					statsd.Gauge(stat, int64(channel.InFlightCount))
 
 					stat = fmt.Sprintf("topic.%s.channel.%s.deferred_count", topic.TopicName, channel.ChannelName)
-					statsd.Gauge(stat, int(channel.DeferredCount))
+					statsd.Gauge(stat, int64(channel.DeferredCount))
 
 					diff = channel.RequeueCount - lastChannel.RequeueCount
 					stat = fmt.Sprintf("topic.%s.channel.%s.requeue_count", topic.TopicName, channel.ChannelName)
-					statsd.Incr(stat, int(diff))
+					statsd.Incr(stat, int64(diff))
 
 					diff = channel.TimeoutCount - lastChannel.TimeoutCount
 					stat = fmt.Sprintf("topic.%s.channel.%s.timeout_count", topic.TopicName, channel.ChannelName)
-					statsd.Incr(stat, int(diff))
+					statsd.Incr(stat, int64(diff))
 
 					stat = fmt.Sprintf("topic.%s.channel.%s.clients", topic.TopicName, channel.ChannelName)
-					statsd.Gauge(stat, len(channel.Clients))
+					statsd.Gauge(stat, int64(len(channel.Clients)))
 				}
 			}
-
 			lastStats = stats
+
+			if *statsdMemStats {
+				var memStats runtime.MemStats
+				runtime.ReadMemStats(&memStats)
+
+				// sort the GC pause array
+				length := 256
+				if len(memStats.PauseNs) <= 256 {
+					length = int(memStats.NumGC)
+				}
+				gcPauses := make(Uint64Slice, length)
+				copy(gcPauses, memStats.PauseNs[:])
+				sort.Sort(gcPauses)
+
+				statsd.Gauge("mem.heap_objects", int64(memStats.HeapObjects))
+				statsd.Gauge("mem.heap_idle_bytes", int64(memStats.HeapIdle))
+				statsd.Gauge("mem.heap_in_use_bytes", int64(memStats.HeapInuse))
+				statsd.Gauge("mem.heap_released_bytes", int64(memStats.HeapReleased))
+				statsd.Gauge("mem.gc_pause_usec_100", int64(percentile(100.0, gcPauses, len(gcPauses))/1000))
+				statsd.Gauge("mem.gc_pause_usec_99", int64(percentile(99.0, gcPauses, len(gcPauses))/1000))
+				statsd.Gauge("mem.gc_pause_usec_95", int64(percentile(95.0, gcPauses, len(gcPauses))/1000))
+				statsd.Gauge("mem.next_gc_bytes", int64(memStats.NextGC))
+				statsd.Incr("mem.gc_runs", int64(memStats.NumGC-lastMemStats.NumGC))
+
+				lastMemStats = memStats
+			}
+
 			statsd.Close()
 		}
 	}
 
 exit:
 	ticker.Stop()
+}
+
+func percentile(perc float64, arr []uint64, length int) uint64 {
+	indexOfPerc := int(math.Ceil(((perc / 100.0) * float64(length)) + 0.5))
+	if indexOfPerc >= length {
+		indexOfPerc = length - 1
+	}
+	return arr[indexOfPerc]
 }
