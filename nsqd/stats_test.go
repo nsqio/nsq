096d09b5c3e96c95ce05ad6860b33c36f4ded22b
elubow <eric@lubow.org>
2014-01-26 21:42:02 -0500

    nsqd: adding test case for client attributes

--- nsqd/stats_test.go
+++ nsqd/stats_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/bitly/go-nsq"
+	"github.com/bitly/nsq/util"
 	"github.com/bmizerany/assert"
+	"github.com/mreiferson/go-snappystream"
 	"io/ioutil"
 	"log"
 	"os"
@@ -35,4 +39,51 @@ func TestStats(t *testing.T) {
 	assert.Equal(t, len(stats[0].Channels), 1)
 	assert.Equal(t, len(stats[0].Channels[0].Clients), 1)
 	log.Printf("stats: %+v", stats)
+}
+
+func TestClientAttributes(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	defer log.SetOutput(os.Stdout)
+
+	userAgent := "Test User Agent"
+
+	*verbose = true
+	options := NewNSQDOptions()
+	options.SnappyEnabled = true
+	tcpAddr, httpAddr, nsqd := mustStartNSQD(options)
+	defer nsqd.Exit()
+
+	conn, err := mustConnectNSQD(tcpAddr)
+	assert.Equal(t, err, nil)
+
+	data := identifyFeatureNegotiation(t, conn, map[string]interface{}{"snappy": true, "user_agent": userAgent})
+	r := struct {
+		Snappy    bool   `json:"snappy"`
+		UserAgent string `json:"user_agent"`
+	}{}
+	err = json.Unmarshal(data, &r)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, r.Snappy, true)
+
+	compressConn := snappystream.NewReader(conn, snappystream.SkipVerifyChecksum)
+
+	w := snappystream.NewWriter(conn)
+
+	rw := readWriter{compressConn, w}
+
+	topicName := "test_client_attributes" + strconv.Itoa(int(time.Now().Unix()))
+	sub(t, rw, topicName, "ch")
+
+	err = nsq.Ready(1).Write(rw)
+	assert.Equal(t, err, nil)
+
+	testUrl := fmt.Sprintf("http://127.0.0.1:%d/stats?format=json", httpAddr.Port)
+
+	statsData, err := util.ApiRequest(testUrl)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	client := statsData.Get("topics").GetIndex(0).Get("channels").GetIndex(0).Get("clients").GetIndex(0)
+	assert.Equal(t, client.Get("user_agent").MustString(), userAgent)
+	assert.Equal(t, client.Get("snappy").MustBool(), true)
 }
