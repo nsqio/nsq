6b5596dcfb7799c0b85a64de82f4ba814b7d1896
David Zarlengo <david@gratafy.com>
2014-05-03 00:13:56 -0700

    uses ; as a topic-channel separator on Windows

--- nsqd/dqname_windows.go
+++ nsqd/dqname_windows.go
@@ -4,7 +4,7 @@ package nsqd
 
 // On Windows, file names cannot contain colons.
 func getBackendName(topicName, channelName string) string {
-	// backend names, for uniqueness, automatically include the topic... <topic>__<channel>
-	backendName := topicName + "__" + channelName
+	// backend names, for uniqueness, automatically include the topic... <topic>;<channel>
+	backendName := topicName + ";" + channelName
 	return backendName
 }
