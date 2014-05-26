9803e5694323d166ecf5c9384da02a03c24445f1
Matt Reiferson <mreiferson@gmail.com>
2014-05-26 17:32:15 -0400

    util: copy some methods from go-nsq

---
+++ util/names.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"regexp"
+)
+
+var validTopicNameRegex = regexp.MustCompile(`^[\.a-zA-Z0-9_-]+$`)
+var validChannelNameRegex = regexp.MustCompile(`^[\.a-zA-Z0-9_-]+(#ephemeral)?$`)
+
+// IsValidTopicName checks a topic name for correctness
+func IsValidTopicName(name string) bool {
+	if len(name) > 32 || len(name) < 1 {
+		return false
+	}
+	return validTopicNameRegex.MatchString(name)
+}
+
+// IsValidChannelName checks a channel name for correctness
+func IsValidChannelName(name string) bool {
+	if len(name) > 32 || len(name) < 1 {
+		return false
+	}
+	return validChannelNameRegex.MatchString(name)
+}
