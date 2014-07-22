0524c8d456b0f26f520d3c4e0c79629bdf20be51
Mat Ryer <mat@stretchr.com>
2014-07-22 13:48:04 -0600

    Post review tweaks
    uses bufio.Reader instead of bufio.Scanner
    added -delimiter flag which defaults to \n
    added --reader-opt which gets passed through
    removed unused code

--- apps/to_nsq/main.go
+++ apps/to_nsq/main.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -18,78 +19,95 @@ import (
 
 var (
 	topic            = flag.String("topic", "", "nsq topic")
-	scan             = flag.String("scan", "lines", "what from stdin to publish (words,lines,bytes or runes)")
+	delimiter        = flag.String("delimiter", "\n", "what from stdin to split on")
 	maxInFlight      = flag.Int("max-in-flight", 200, "max number of messages to allow in flight")
-	nsqdTCPAddrs     = util.StringArray{}
-	lookupdHTTPAddrs = util.StringArray{}
 	destNsqdTCPAddrs = util.StringArray{}
+	readerOpts       = util.StringArray{}
 )
 
 func init() {
+	flag.Var(&readerOpts, "reader-opt", "option to passthrough to nsq.Consumer (may be given multiple times)")
 	flag.Var(&destNsqdTCPAddrs, "nsqd-tcp-address", "destination nsqd TCP address (may be given multiple times)")
 }
 
 func main() {
 
+	// handle stopping
+	stopChan := make(chan bool)
 	termChan := make(chan os.Signal, 1)
 	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 
+	// parse flags
 	flag.Parse()
-
 	if len(*topic) == 0 {
 		fatal("Must specify a valid topic")
 	}
+	if len(*delimiter) != 1 {
+		fatal("Delimiter must be a single byte")
+	}
 
+	// prepare the configuration
 	wcfg := nsq.NewConfig()
 	wcfg.UserAgent = fmt.Sprintf("to_nsq/%s go-nsq/%s", util.BINARY_VERSION, nsq.VERSION)
+	if err := util.ParseReaderOpts(wcfg, readerOpts); err != nil {
+		fatal(err)
+	}
 	wcfg.MaxInFlight = *maxInFlight
 
+	// make the producers
 	producers := make(map[string]*nsq.Producer)
 	for _, addr := range destNsqdTCPAddrs {
 		producer, err := nsq.NewProducer(addr, wcfg)
 		if err != nil {
 			fatal("failed creating producer", err)
 		}
+		producer.SetLogger(log.New(os.Stderr, "", log.LstdFlags), nsq.LogLevelInfo)
 		producers[addr] = producer
 		defer producer.Stop()
 	}
-
 	if len(producers) == 0 {
 		fatal("Must specify at least one nsqd-tcp-address")
 	}
 
 	// scan the input
-	s := bufio.NewScanner(os.Stdin)
-	s.Split(splitFunc())
+	r := bufio.NewReader(os.Stdin)
+	delim := []byte(*delimiter)[0]
 	go func() {
-		for s.Scan() {
-			for _, producer := range producers {
-				if err := producer.Publish(*topic, s.Bytes()); err != nil {
-					log.Fatalln(err)
+		var readErr error = nil
+		var line []byte
+		for readErr == nil {
+
+			line, readErr = r.ReadBytes(delim)
+			if readErr == nil || readErr == io.EOF {
+				if len(line) > 0 {
+					line = line[:len(line)-1]
 				}
+				if len(line) > 0 {
+					for _, producer := range producers {
+						fmt.Println(string(line))
+						if err := producer.Publish(*topic, line); err != nil {
+							log.Fatalln(err)
+						}
+					}
+				}
+				if readErr == io.EOF {
+					stopChan <- true
+				}
+			} else {
+				// real error
+				stopChan <- true
+				fatal(readErr)
 			}
+
 		}
 	}()
 
-	<-termChan
-}
-
-// splitFunc gets the split function to use when scanning
-// from the input.
-func splitFunc() bufio.SplitFunc {
-	switch *scan {
-	case "words":
-		return bufio.ScanWords
-	case "lines":
-		return bufio.ScanLines
-	case "runes":
-		return bufio.ScanRunes
-	case "bytes":
-		return bufio.ScanBytes
-	default:
-		fatal("scan must be words, lines, bytes or runes")
+	// wait for things to finish
+	select {
+	case <-termChan:
+	case <-stopChan:
 	}
-	return nil
+
 }
 
 // fatal writes an error and exits
