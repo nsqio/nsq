8661ee75f69369eb6155c2f1688d2ae75b111da6
Matt Reiferson <mreiferson@gmail.com>
2014-08-16 09:58:20 -0400

    nsqd: split apart backend queue code for testing

---
+++ nsqd/backend_queue.go
@@ -0,0 +1,12 @@
+package nsqd
+
+// BackendQueue represents the behavior for the secondary message
+// storage system
+type BackendQueue interface {
+	Put([]byte) error
+	ReadChan() chan []byte // this is expected to be an *unbuffered* channel
+	Close() error
+	Delete() error
+	Depth() int64
+	Empty() error
+}
