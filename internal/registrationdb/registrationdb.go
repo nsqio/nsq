0ed336795acae278a1f2b648b6b67ed4c0e9f95a
Stephen Searles <stephen.searles@shipwire.com>
2015-01-17 13:00:50 -0800

    registrationdb: docs

--- internal/registrationdb/registrationdb.go
+++ internal/registrationdb/registrationdb.go
@@ -1,3 +1,12 @@
+// Package registrationdb tracks a set of producers for a various registration.
+// Registrations fall under a category, and may be distinguished with keys and
+// subkeys.
+//
+// Examples of usage include NSQ topic, channel, and client registrations. These
+// registrations are added under separate categories, so a producer will be
+// registered for each of these things independently. This allows keeping
+// information about things like topic existence and client existince separate
+// while not requiring additional memory to store producers multiple times.
 package registrationdb
 
 import (
@@ -7,24 +16,30 @@ import (
 	"time"
 )
 
+// RegistrationDB stores Producers keyed by Registrations
 type RegistrationDB struct {
 	mtx  sync.RWMutex
 	data map[Registration]Producers
 }
 
+// Registration is key for RegistrationDB identified by a category, key, and
+// subkey
 type Registration struct {
 	Category string
 	Key      string
 	SubKey   string
 }
 
+// String returns a human-readable string
 func (r Registration) String() string {
 	return fmt.Sprintf("category:%s key:%s subkey:%s",
 		r.Category, r.Key, r.SubKey)
 }
 
+// Registrations is a list of Registration
 type Registrations []Registration
 
+// PeerInfo contains the metadata for a Peer
 type PeerInfo struct {
 	LastUpdate       int64  `json:"-"`
 	ID               string `json:"-"`
@@ -36,34 +51,42 @@ type PeerInfo struct {
 	Version          string `json:"version"`
 }
 
+// Producer is a unique, per Registration, Peer
 type Producer struct {
 	*PeerInfo
 	tombstoned   bool
 	tombstonedAt time.Time
 }
 
+// Producers is a list of Producer
 type Producers []*Producer
 
+// Producer returns a human-readable string
 func (p Producer) String() string {
 	return fmt.Sprintf("%s [%d, %d]",
 		p.BroadcastAddress, p.TCPPort, p.HTTPPort)
 }
 
+// Tombstone marks this producer as tombstoned
 func (p *Producer) Tombstone() {
 	p.tombstoned = true
 	p.tombstonedAt = time.Now()
 }
 
+// IsTombstoned returns a boolean indicating the tombstone status of this Producer
+// for the given lifetime
 func (p *Producer) IsTombstoned(lifetime time.Duration) bool {
 	return p.tombstoned && time.Now().Sub(p.tombstonedAt) < lifetime
 }
 
+// New returns a new instance of RegistrationDB
 func New() *RegistrationDB {
 	return &RegistrationDB{
 		data: make(map[Registration]Producers),
 	}
 }
 
+// Debug returns a map containing the metadata for all registrations and producers
 func (r *RegistrationDB) Debug() map[string][]map[string]interface{} {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
@@ -90,7 +113,8 @@ func (r *RegistrationDB) Debug() map[string][]map[string]interface{} {
 	return data
 }
 
-// add a registration key
+// AddRegistration creates an empty list of producers under a given registration
+// if it does not exist
 func (r *RegistrationDB) AddRegistration(k Registration) {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -100,7 +124,7 @@ func (r *RegistrationDB) AddRegistration(k Registration) {
 	}
 }
 
-// add a producer to a registration
+// AddProducer adds a producer to a registration set
 func (r *RegistrationDB) AddProducer(k Registration, p *Producer) bool {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -117,7 +141,7 @@ func (r *RegistrationDB) AddProducer(k Registration, p *Producer) bool {
 	return !found
 }
 
-// remove a producer from a registration
+// RemoveProducer removes a producer from a registration set
 func (r *RegistrationDB) RemoveProducer(k Registration, id string) (bool, int) {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -139,7 +163,7 @@ func (r *RegistrationDB) RemoveProducer(k Registration, id string) (bool, int) {
 	return removed, len(cleaned)
 }
 
-// remove a Registration and all it's producers
+// RemoveRegistration removes all producers for a registration
 func (r *RegistrationDB) RemoveRegistration(k Registration) {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -150,6 +174,10 @@ func (r *RegistrationDB) needFilter(key string, subkey string) bool {
 	return key == "*" || subkey == "*"
 }
 
+// FindRegistrations finds all the registrations (sets of producers) that match
+// the given category, key, and subkey.
+//
+// The key and subkey may be given as wildcards (*).
 func (r *RegistrationDB) FindRegistrations(category string, key string, subkey string) Registrations {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
@@ -170,6 +198,10 @@ func (r *RegistrationDB) FindRegistrations(category string, key string, subkey s
 	return results
 }
 
+// FindProducers finds all the producers that are registered under the given
+// category, key, and subkey.
+//
+// The key and subkey may be given as wildcards (*).
 func (r *RegistrationDB) FindProducers(category string, key string, subkey string) Producers {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
@@ -197,6 +229,8 @@ func (r *RegistrationDB) FindProducers(category string, key string, subkey strin
 	return results
 }
 
+// LookupRegistrations finds the list of registrations (sets of producers) that
+// contain the producer with the ID given
 func (r *RegistrationDB) LookupRegistrations(id string) Registrations {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
@@ -212,6 +246,10 @@ func (r *RegistrationDB) LookupRegistrations(id string) Registrations {
 	return results
 }
 
+// TouchProducer finds the the producer with ID id under the registration k and
+// updates its LastUpdate to the current time.
+//
+// If the producer was found, the function returns true.
 func (r *RegistrationDB) TouchProducer(k Registration, id string) bool {
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
@@ -229,6 +267,9 @@ func (r *RegistrationDB) TouchProducer(k Registration, id string) bool {
 	return false
 }
 
+// IsMatch matches a category, key, and subkey with a registration.
+//
+// The key and subkey may be given as wildcards (*).
 func (k Registration) IsMatch(category string, key string, subkey string) bool {
 	if category != k.Category {
 		return false
@@ -242,6 +283,8 @@ func (k Registration) IsMatch(category string, key string, subkey string) bool {
 	return true
 }
 
+// Filter returns a new list of registrations that match the given category,
+// key, and subkey.
 func (rr Registrations) Filter(category string, key string, subkey string) Registrations {
 	output := Registrations{}
 	for _, k := range rr {
@@ -252,6 +295,7 @@ func (rr Registrations) Filter(category string, key string, subkey string) Regis
 	return output
 }
 
+// Keys returns the keys of rr as a slice of strings.
 func (rr Registrations) Keys() []string {
 	keys := make([]string, len(rr))
 	for i, k := range rr {
@@ -260,6 +304,7 @@ func (rr Registrations) Keys() []string {
 	return keys
 }
 
+// SubKeys returns the subkeys of rr as a slice of strings.
 func (rr Registrations) SubKeys() []string {
 	subkeys := make([]string, len(rr))
 	for i, k := range rr {
@@ -268,6 +313,9 @@ func (rr Registrations) SubKeys() []string {
 	return subkeys
 }
 
+// FilterByActive returns a new list of producers that include elements of pp
+// which have been active since inactivityTimeout and have not been tombstoned
+// since tombstoneLifetime.
 func (pp Producers) FilterByActive(inactivityTimeout time.Duration, tombstoneLifetime time.Duration) Producers {
 	now := time.Now()
 	results := Producers{}
