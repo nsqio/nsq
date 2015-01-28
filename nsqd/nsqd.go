7c6c5ddfdd61a59df5f2a7337653519a125d20db
Stephen Searles <stephen.searles@shipwire.com>
2015-01-28 13:53:22 -0800

    nsqd: only spin up worker goroutines if gossip is enabled

--- nsqd/nsqd.go
+++ nsqd/nsqd.go
@@ -301,10 +301,9 @@ func (n *NSQD) Main() {
 			os.Exit(1)
 		}
 		n.serf = serf
+		n.waitGroup.Wrap(func() { n.serfEventLoop() })
+		n.waitGroup.Wrap(func() { n.gossipLoop() })
 	}
-
-	n.waitGroup.Wrap(func() { n.serfEventLoop() })
-	n.waitGroup.Wrap(func() { n.gossipLoop() })
 }
 
 func (n *NSQD) LoadMetadata() {
@@ -614,12 +613,14 @@ func (n *NSQD) Notify(v interface{}) {
 		}
 	})
 
-	n.waitGroup.Wrap(func() {
-		select {
-		case <-n.exitChan:
-		case n.gossipChan <- v:
-		}
-	})
+	if n.serf != nil {
+		n.waitGroup.Wrap(func() {
+			select {
+			case <-n.exitChan:
+			case n.gossipChan <- v:
+			}
+		})
+	}
 }
 
 // channels returns a flat slice of all channels in all topics
