6458982a6f227e5e8e08013d32013f519b69f776
Jehiah Czebotar <jehiah@gmail.com>
2015-09-21 13:56:45 -0400

    nsqadmin: fix nsqd backwards compatibility when querying nsqd directly

--- apps/nsqadmin/main.go
+++ apps/nsqadmin/main.go
@@ -10,10 +10,10 @@ import (
 	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/mreiferson/go-options"
 	"github.com/nsqio/nsq/internal/app"
 	"github.com/nsqio/nsq/internal/version"
 	"github.com/nsqio/nsq/nsqadmin"
-	"github.com/mreiferson/go-options"
 )
 
 var (
