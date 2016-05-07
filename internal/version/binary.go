1502392209387336d2136b98982496f9d1c46faf
Matt Reiferson <mreiferson@gmail.com>
2016-05-07 09:51:28 -0700

    bump v0.3.8-rc1

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "0.3.7"
+const Binary = "0.3.8-rc1"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
