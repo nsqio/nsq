77ed5808112e6481a58d6a00b3c072a32c118ced
Vincent Lee <cool8511@gmail.com>
2016-05-20 11:06:37 +0800

    add HA string for version

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "0.3.7"
+const Binary = "0.3.7 HA.1.0"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
