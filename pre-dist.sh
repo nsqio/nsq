64aa3fdc4e67b60dd9c8cf89a3b6e7d661e84b48
Vincent Lee <cool8511@gmail.com>
2016-06-07 10:45:57 +0800

    package script adjust

--- pre-dist.sh
+++ pre-dist.sh
@@ -16,31 +16,16 @@
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+rm -rf   $DIR/dist/docker
+mkdir -p $DIR/dist/docker
+rm -rf   $DIR/.godeps
+mkdir -p $DIR/.godeps
 export GOPATH=$DIR/.godeps:$GOPATH
+GOPATH=$DIR/.godeps gpm install
 
 arch=$(go env GOARCH)
 version=$(awk '/const Binary/ {print $NF}' < $DIR/internal/version/binary.go | sed 's/"//g')
 goversion=$(go version | awk '{print $3}')
 
-for os in linux darwin freebsd windows; do
-    echo "... building v$version for $os/$arch"
-    BUILD=$(mktemp -d -t nsqXXXXXX)
-    TARGET="nsq-$version.$os-$arch.$goversion"
-    GOOS=$os GOARCH=$arch CGO_ENABLED=0 \
-        make DESTDIR=$BUILD PREFIX=/$TARGET install
-    pushd $BUILD
-    if [ "$os" == "linux" ]; then
-        cp -r $TARGET/bin $DIR/dist/docker/
-    fi
-    tar czvf $TARGET.tar.gz $TARGET
-    mv $TARGET.tar.gz $DIR/dist
-    popd
-    make clean
-    rm -r $BUILD
-done
-
-docker build -t nsqio/nsq:v$version .
-if [[ ! $version == *"-"* ]]; then
-    echo "Tagging nsqio/nsq:v$version as the latest release."
-    docker tag -f nsqio/nsq:v$version nsqio/nsq:latest
-fi
+echo "... running tests"
+./test.sh
