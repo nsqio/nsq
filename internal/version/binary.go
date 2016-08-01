f39df0dad34cf3c88c97ac0304fd6230537421af
Vincent Lee <cool8511@gmail.com>
2016-08-01 15:26:25 +0800

    update version

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "0.3.7-HA.1.2"
+const Binary = "0.3.7-HA.1.3"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
