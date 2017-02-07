7588327fb10d4add54142d611d5d71f778a5b7fd
Pierce Lopez <pierce.lopez@gmail.com>
2017-02-07 15:50:01 -0500

    nsqd: GUID benchmark update

--- nsqd/guid_test.go
+++ nsqd/guid_test.go
@@ -23,9 +23,20 @@ func BenchmarkGUIDUnsafe(b *testing.B) {
 }
 
 func BenchmarkGUID(b *testing.B) {
+	var okays, errors, fails int64
+	var previd guid
 	factory := &guidFactory{}
 	for i := 0; i < b.N; i++ {
-		id, _ := factory.NewGUID()
+		id, err := factory.NewGUID()
+		if err != nil {
+			errors++
+		} else if id == previd {
+			fails++
+			b.Fail()
+		} else {
+			okays++
+		}
 		id.Hex()
 	}
+	b.Logf("okays=%d errors=%d bads=%d", okays, errors, fails)
 }
