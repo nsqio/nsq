6f9b481fe19f0d9607b5ecbccb4b853596a8066a
daniel mican <dmican@daniels-MacBook-Pro.local>
2017-07-30 21:01:40 -0400

    dogstatsd contrib: initializer table

--- contrib/nsqd.go
+++ contrib/nsqd.go
@@ -3,6 +3,7 @@ package contrib
 import (
 	"github.com/nsqio/nsq/internal/lg"
 	"github.com/nsqio/nsq/nsqd"
+	"strings"
 )
 
 type INSQD interface {
@@ -10,6 +11,8 @@ type INSQD interface {
 	GetStats() []nsqd.TopicStats
 	AddModuleGoroutine(addonFn func(exitChan chan int))
 }
+
+type initializer func([]string, INSQD) INSQDAddon
 
 type INSQDAddon interface {
 	Enabled() bool
@@ -31,13 +34,31 @@ func (as *NSQDAddons) Start() {
 // Initializes addons that have options set
 func NewEnabledNSQDAddons(contribOpts []string, n INSQD) *NSQDAddons {
 	var activeAddons []INSQDAddon
+	var hasOpt bool
+
+	initializers := map[string]initializer{
+		"dogstatsd": NewNSQDDogStatsd,
+	}
 
 	n.Logf(nsqd.LOG_INFO, "Addons Initializing")
 
-	// ask each addon if it should be initialize
-	dogStats := NewNSQDDogStatsd(contribOpts, n)
-	if dogStats.Enabled() {
-		activeAddons = append(activeAddons, dogStats)
+	for k, initializer := range initializers {
+		// check if any of the options contains this addon's expected argument
+		hasOpt = false
+
+		for _, opt := range contribOpts {
+			if strings.Contains(opt, k) {
+				hasOpt = true
+				break
+			}
+		}
+
+		if hasOpt {
+			addon := initializer(contribOpts, n)
+			if addon.Enabled() {
+				activeAddons = append(activeAddons, addon)
+			}
+		}
 	}
 
 	return &NSQDAddons{
