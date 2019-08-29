81b8b9ea89ec0e6a49434b6e40cd77045d734782
Pierce Lopez <pierce.lopez@gmail.com>
2019-08-29 02:15:49 -0400

    bump v1.2.1-alpha

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "1.2.0"
+const Binary = "1.2.1-alpha"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
