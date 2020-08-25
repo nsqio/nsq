6187e23afe73a61412af0a53fb886307589427bb
Chaiyq <1025317432@qq.com>
2020-08-25 21:34:48 +0800

    add a interface to get topic information for app developer to make a decision

--- nsqd/http_test.go
+++ nsqd/http_test.go
@@ -530,7 +530,7 @@ func TestHTTPV1GetTopicInfo(t *testing.T) {
 	test.Equal(t, "nsq; version=1.0", resp.Header.Get("X-NSQ-Content-Type"))
 
 	topicRsp := nsqd.GetSpecialTopicsInfo(topicName)
-	test.Nil(t, topicRsp)
+	test.NotNil(t, topicRsp)
 
 	url = fmt.Sprintf("http://%s/topic/delete?topic=%s", httpAddr, topicName)
 	resp, err = http.Post(url, "application/json", nil)
