242ed67c5badc6fed57ad34ae42d2bd531611137
Vincent R <vbiret@cryptocompare.com>
2020-09-30 09:24:45 +0100

    feat: new blob path strategy

--- apps/nsq_to_file/azure_uploader.go
+++ apps/nsq_to_file/azure_uploader.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"path"
+	"strings"
+	"time"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/nsqio/nsq/internal/lg"
@@ -47,14 +50,78 @@ func (au *AzureUploader) listenToEventsChannel() {
 
 	for {
 		select {
-		case filename := <-au.events:
-			// parse file name for pair and details
-			blobURL := au.containerURL.NewBlockBlobURL(filename)
-			file, err := os.Open(filename)
+		case filePath := <-au.events:
+
+			file, err := os.Open(filePath)
+
+			stat, err := file.Stat()
 			if err != nil {
 				au.logf(lg.ERROR, "failed to open file in AzureUploader, listenToEventsChannel: %s", err)
 				continue
 			}
+
+			// Check file size, if empty skip processing
+			if stat.Size() == 0 {
+				au.logf(lg.WARN, "File is empty, skipping")
+				continue
+			}
+
+			if err != nil {
+				au.logf(lg.ERROR, "failed to open file in AzureUploader, listenToEventsChannel: %s", err)
+				continue
+			}
+
+			// eg files:
+			// 8-bithumb-BTC-KRW.cc-md-nsq-output-05-000000.2020-09-29_13.log.gz
+			// 30-bithumb-OMG-KRW.cc-md-nsq-output-05-000000.2020-09-29_13.log.gz
+			// 0-bithumb.cc-md-nsq-output-05-000000.2020-09-29_13.log.gz
+
+			fileName := path.Base(filePath)
+			dashSplitted := strings.Split(fileName, "-")
+			exchangeName := strings.ToLower(dashSplitted[1])
+
+			// Begin blob path string construction
+			blobPath := exchangeName
+
+			if strings.HasPrefix(fileName, "8-") || strings.HasPrefix(fileName, "30-") {
+				// Level2
+
+				base := strings.ToLower(dashSplitted[2])
+				quote := strings.ToLower(dashSplitted[3])
+
+				blobPath = exchangeName
+				if dashSplitted[0] == "8" {
+					blobPath += "/obupdate/"
+				} else {
+					blobPath += "/topofbook/"
+				}
+
+				blobPath += base + "-" + quote + "/"
+
+			} else if strings.HasPrefix(fileName, "0-") {
+				// Trades
+				blobPath += "/trade/"
+
+			} else {
+				// Unsupported
+				au.logf(lg.ERROR, "Unsupported file type, filename [%s]", fileName)
+				continue
+			}
+
+			// Parse date
+			dateString := strings.SplitAfterN(fileName, ".", 2)[0]
+			dateLayout := "2006-01-02_15"
+
+			dateTime, err := time.Parse(dateLayout, dateString)
+			if err != nil {
+				au.logf(lg.ERROR, "parsing date time, %s", err)
+				continue
+			}
+
+			blobPath += fmt.Sprintf("%d.gz", dateTime.Unix())
+
+			// Create Block Blob Path
+			blobURL := au.containerURL.NewBlockBlobURL(blobPath)
 
 			_, err = azblob.UploadFileToBlockBlob(context.Background(), file, blobURL, azblob.UploadToBlockBlobOptions{
 				BlockSize:   512 * 1024,
