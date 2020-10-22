ef1844fa1b4d4d3df3c9adb3b1d2cd6feca47b04
loeken <loeken@internetz.me>
2020-10-22 13:53:32 +0200

    Update before-install.sh

--- deploy/systemd/before-install.sh
+++ deploy/systemd/before-install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 id -u cc &>/dev/null || useradd cc -s /bin/bash -p '*'
+mkdir -p /opt/nsq_to_file_storage
+mkdir -p /opt/tmp_nsq_to_file
+chown -R cc:cc /opt/nsq_to_file_storage
+chown -R cc:cc /opt/tmp_nsq_to_file
