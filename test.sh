ca13d88277b3c40b00a501e629e2114154e30c6f
Matt Reiferson <mreiferson@gmail.com>
2021-03-28 11:13:31 -0700

    *: fix test for standard sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 
 GOMAXPROCS=1 go test -timeout 90s ./...
 
-if [ amd64 == "$GOARCH" ] || [ arm64 == "$GOARCH" ]; then
+if [ "$GOARCH" = "amd64" ] || [ "$GOARCH" = "arm64" ]; then
     # go test: -race is only supported on linux/amd64, linux/ppc64le,
     # linux/arm64, freebsd/amd64, netbsd/amd64, darwin/amd64 and windows/amd64
     GOMAXPROCS=4 go test -timeout 90s -race ./...
