37c281745280f363938c505a2989e61643d04043
Pierce Lopez <pierce.lopez@gmail.com>
2021-05-12 11:22:37 -0400

    fixup formatting

--- internal/auth/authorizations.go
+++ internal/auth/authorizations.go
@@ -107,7 +107,7 @@ func QueryAuthd(authd string, remoteIP string, tlsEnabled bool, commonName strin
 	v.Set("common_name", commonName)
 
 	var endpoint string
-	if strings.Contains(authd,"://"){
+	if strings.Contains(authd, "://"){
 		endpoint = fmt.Sprintf("%s?%s", authd, v.Encode())
 	} else {
 		endpoint = fmt.Sprintf("http://%s/auth?%s", authd, v.Encode())
